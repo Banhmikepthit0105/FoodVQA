d7db3ebd86d4599384e38a3b751838a8456ea7ee
Huy Trieu <67747576+huyg1108@users.noreply.github.com>
2025-03-07 15:52:56 +0700

    code to evaluate MiniCPM-o 2.6 model

---
+++ eval_MiniCPM.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from PIL import Image
+import os
+import torch
+from transformers import AutoModel, AutoTokenizer
+
+## REQUIREMENT: pip install vector_quantize_pytorch vocos
+
+def main():
+    error_counter = 0
+    working_dir = '/kaggle/input/foodvqa'
+    image_dir = os.path.join(working_dir, 'assets', 'assets')
+    test_path = os.path.join(working_dir, 'test.csv')
+
+    test_df = pd.read_csv(test_csv_path, sep=' ', header=0)
+    result_df = pd.DataFrame(columns=['Image', 'Question', 'Answer', 'Predict'])
+    image_extensions = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG']
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = AutoModel.from_pretrained(
+        'openbmb/MiniCPM-o-2_6',
+        trust_remote_code=True,
+        attn_implementation='sdpa',
+        torch_dtype=torch.bfloat16,
+        init_vision=True,
+        init_audio=True,
+        init_tts=True
+    ).to(device).eval()
+
+    tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-o-2_6', trust_remote_code=True)
+
+    # Loop each row
+    for idx, row in test_df.iterrows():
+        img_name = row['Image']
+        image_path = None
+        
+        for ext in image_extensions:
+            temp_path = os.path.join(image_dir, f"{img_name}{ext}")
+            if os.path.exists(temp_path):
+                image_path = temp_path
+                break
+        
+        if image_path is None:
+            error_counter += 1
+        
+        image = Image.open(image_path).convert('RGB')
+
+        question = row['Question']
+        true_answer = row['Answer']
+        
+        msgs = [{'role': 'user', 'content': [image, question]}]
+        predict = model.chat(msgs=msgs, tokenizer=tokenizer)
+        
+        result_df.loc[idx] = [img_name, question, true_answer, predict]
+        
+    result_df.to_csv('answer_MiniCPM.csv', index=False)
+    print(error_counter)
+    print(result_df)
+
+if __name__ == "__main__":
+    main()
